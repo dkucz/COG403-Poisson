4792178527e60d42a677afc7dba21e4248bfaaa2
Dominik Roy Kucz <kuczdomi@teach.cs.toronto.edu>
2025-03-31 23:09:22 -0400

    Added initial simulation code, currently doesn't accumulate properly

--- agent.py
+++ agent.py
@@ -62,7 +62,7 @@ class AccumulationRule2(FixedRules):
 
     def __init__(self, name, p, r, c, d, v, *, sd = 1, threshold):
         super().__init__(name, p, r, c, d, v, sd=sd)
-        self.accumulator = Site(self.rhs.td.main.index, {}, 0)
+        self.accumulator = Site(self.rules.rhs.td.main.index, {}, 0)
         self.threshold = threshold
 
     def update_accumulator(self, 
@@ -70,7 +70,7 @@ class AccumulationRule2(FixedRules):
         priority: int = Priority.PROPAGATION
     ) -> None:
         self.system.schedule(self.update_accumulator,
-            self.accumulator.add_inplace(self.rhs.td.main[0]),
+            self.accumulator.add_inplace(self.rules.rhs.td.main[0]),
             dt=dt, priority=priority)
 
     # self.rhs.td.main[0] represents the data in the site currently 
@@ -82,11 +82,11 @@ class AccumulationRule2(FixedRules):
 
     def resolve(self, event: Event) -> None:
         super().resolve(event)
-        if event.source == self.rhs.td.update:
+        if event.source == self.rules.rhs.td.update:
             self.update_accumulator()
         
         if event.source == self.update_accumulator:
-            curr_accumulator = self.rhs.td.main[0]
+            curr_accumulator = self.rules.rhs.td.main[0]
 
             if curr_accumulator.max().c > self.threshold:
                 self.choice.select()
@@ -96,10 +96,10 @@ class AccumulationRule2(FixedRules):
 
 p = Family()
 data = PRWData()
-with Agent('agent', d=data) as agent:
-    data_in = Input("data_in", (data.io.input, data.direction))
-    accumulation_rules = AccumulationRule2("accumulation_rules", p=p, r=data, c=data, d=data, v=data, sd=1e-4, threshold=0.75)
-    choice = Choice('choice', p=p, d=data.io.output, v=data.direction, sd=1e-4)
+with Agent('agent', d=data, p=p) as agent:
+    data_in = Input("data_in", (data, data))
+    accumulation_rules = AccumulationRule2("accumulation_rules", p=p, r=data, c=data, d=data, v=data, sd=1e-4, threshold=0.01)
+    choice = Choice('choice', p, (data.io.input, data.direction), sd=1e-4)
     accumulation_rules.rules.lhs.bu.input = data_in.main
     choice.input = accumulation_rules.rules.rhs.td.main
 
@@ -111,3 +111,31 @@ rule_defs = [
     >>
     + io.output ** direction("X")
 ]
+
+trials = [
+    + io.input ** direction.left,
+
+    + io.input ** direction.right
+]
+
+results = []
+
+dt = timedelta(seconds=1)
+
+accumulation_rules.rules.compile(*rule_defs)
+
+trial_one = trials.pop(0)
+
+data_in.send(trial_one)
+
+accumulation_rules.trigger()
+
+while agent.system.queue:
+    event = agent.system.advance()
+    print(accumulation_rules.rules.rhs.td.main[0].max())
+    if event.source == choice.select:
+        results.append((event.time, choice.poll()))
+    else:
+        data_in.send(trial_one)
+
+print(results)
